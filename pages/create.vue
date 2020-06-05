b9f7784337beb7f67691977c4e401caa5f3c3306
luml <1520622848@qq.com>
2020-06-05 10:48:43 +0800

    feat(add simple table and select element)

--- pages/create.vue
+++ pages/create.vue
@@ -1,5 +1,31 @@
 <template>
     <div>
       <h1>Create an event</h1>
+      <table>
+        <thead>
+            <tr>
+                <th colspan="2">The table header</th>
+            </tr>
+        </thead>
+      <tbody>
+          <tr>
+              <td>The table body</td>
+              <td>with two columns</td>
+          </tr>
+      </tbody>
+</table>
     </div>
 </template>
+
+<style scoped>
+table,
+td {
+    border: 1px solid #333;
+}
+
+thead,
+tfoot {
+    background-color: #333;
+    color: #fff;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
     <div>
       <h1>Event</h1>
+      <div>
+        <select name="events" id="eventName">
+          <option value="Just">🎥filming</option>
+          <option value="Say">🎙singing</option>
+          <option value="Hey">🎛Radioing</option>
+        </select>
+      </div>
     </div>
 </template>
+
+<style scoped>
+select > option {
+  color : #39b982;
+  font-size : 1rem;
+}
+</style>
