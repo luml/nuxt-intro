e9948e2fb2692087856b7a6da75b18314dd50999
luml <1520622848@qq.com>
2020-06-05 11:55:08 +0800

    feat(API: asyncData方法)

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-      <h1>Event</h1>
+      <h1>Hey {{name}}</h1>
+      <!-- <h1>Reps: {{Reps}}</h1> -->
       <div>
         <select name="events" id="eventName">
           <option value="Just">🎥filming</option>
@@ -10,6 +11,31 @@
       </div>
     </div>
 </template>
+
+
+<script>
+import axios from 'axios';
+
+export default {
+  data () {
+    return {name: 'Default'}
+  },
+  asyncData (context) {
+    return {name: 'There'}
+  },
+  // fetch ({Reps}) {
+  //   return axios.get('https://api.github.com/users/luml/repos')
+  //   .then((res) => {
+  //     Reps.commit(res.data)
+  //     console.log(res.data)
+  //   })
+  // },
+  // head () {
+
+  // }
+}
+</script>
+
 
 <style scoped>
 select > option {
